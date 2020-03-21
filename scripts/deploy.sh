cf4ab7f0f76fed3b3e395def4e874fe9b078aaa2
Gabriele Farei <JayFarei@users.noreply.github.com>
2020-03-21 22:37:39 +0000

    restructure folder + deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,9 @@
 
 # this script will copy the content of the development folder in the ubersicht folder - overriding existing files
 
-# source folder: $HOME/documents/development/superG_bar_ubersicht
+# source folder: $HOME/documents/development/superG_bar_ubersicht/widgets/
 # destination folder: $HOME/Library/Application\ Support/Übersicht/widgets/superG_bar
 
-#
+rsync -r /Users/gabrielefarei/Documents/Development/superG_bar_ubersicht/widgets/ $HOME/Library/Application\ Support/Übersicht/widgets/superG_bar
+
 
